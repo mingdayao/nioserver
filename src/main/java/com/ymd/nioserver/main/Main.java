a579da442e8bc9f85f450f688d92785903bab1f8
mingdayao <yaomingdagongle@163.com>
2016-12-27 15:12:09 +0800

    Update Main.java

--- src/main/java/com/ymd/nioserver/main/Main.java
+++ src/main/java/com/ymd/nioserver/main/Main.java
@@ -1,9 +1,15 @@
 package com.ymd.nioserver.main;
 
+/**
+ * 
+ * This will help to take as wrapper to call fee engine
+ *
+ */
 public class Main {
 	
 	public static void main(String[] args) {
-		
+		Server server = new Server(9999, new TcpMessageReaderFactory(), new SimpleMessageProcessor());
+		server.startUp();
 	}
 	
 }
